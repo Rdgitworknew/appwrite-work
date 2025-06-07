40c675fa58056587bd4f277f5883cac3ef877888
Harry <rd337513@gmail.com>
2025-06-07 20:49:55 +0530

    rana : update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,6 @@ import { Login as loginComponent } from '../components'
 function Login() {
   return (
     <div className='py-5'>
-      test production
         <loginComponent />
     </div>
   )
